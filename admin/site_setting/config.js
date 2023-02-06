0c1883679c8eb2ae8ab5a2390cbc24d16c907206
Hamid Alinia <a.goodlookingboy@gmail.com>
2023-02-06 16:46:20 +0330

    resolve in stock products show

--- admin/site_setting/config.js
+++ admin/site_setting/config.js
@@ -1,5 +1,5 @@
-window.BASE_URL='https://v2.golafshan.ir';
-window.ADMIN_URL='https://v2.golafshan.ir';
-window.ADMIN_ROUTE='https://v2.golafshan.ir/admin';
-window.SHOP_URL='https://v2.golafshan.ir/';
-window.THEME_URL='https://v2.golafshan.ir/theme/';
+window.BASE_URL='https://cinemachoob.shop';
+window.ADMIN_URL='https://cinemachoob.shop';
+window.ADMIN_ROUTE='https://cinemachoob.shop/admin';
+window.SHOP_URL='https://cinemachoob.shop/';
+window.THEME_URL='https://cinemachoob.shop/theme/';
