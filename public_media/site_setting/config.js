72d50864be9a6b362753de61fc6b0692b30cac45
Hamid Alinia <a.goodlookingboy@gmail.com>
2023-02-13 14:19:34 +0330

    add meta title and description

---
+++ public_media/site_setting/config.js
@@ -0,0 +1,5 @@
+window.BASE_URL='http://localhost:3000';
+window.ADMIN_URL='http://localhost:3002';
+window.THEME_URL='http://localhost:3000/theme';
+window.ADMIN_ROUTE='http://localhost:3000/admin';
+window.SHOP_URL='http://localhost:3000/';
--- theme/site_setting/config.js
+++ theme/site_setting/config.js
@@ -1,8 +1,8 @@
-window.BASE_URL='https://gilbanu.ir';
-window.ADMIN_URL='https://gilbanu.ir';
-window.SHOP_URL='https://gilbanu.ir/';
-window.THEME_URL='https://gilbanu.ir/theme/';
-// window.BASE_URL='https://gilbanu.ir';
-// window.ADMIN_URL='https://gilbanu.ir';
-// window.SHOP_URL='https://gilbanu.ir/';
-// window.THEME_URL='https://gilbanu.ir/theme/';
+window.BASE_URL='https://fixche.shop';
+window.ADMIN_URL='https://fixche.shop';
+window.SHOP_URL='https://fixche.shop/';
+window.THEME_URL='https://fixche.shop/theme/';
+// window.BASE_URL='http://fixche.shop';
+// window.ADMIN_URL='http://fixche.shop';
+// window.SHOP_URL='http://fixche.shop/';
+// window.THEME_URL='http://fixche.shop/theme/';
