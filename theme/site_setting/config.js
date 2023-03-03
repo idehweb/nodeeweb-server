c67a373adbafab5493bdb8aeca260026904fbbef
Hamid Alinia <a.goodlookingboy@gmail.com>
2023-03-03 17:04:55 +0330

    add paddingTop and paddingBottom support

--- theme/site_setting/config.js
+++ theme/site_setting/config.js
@@ -1,7 +1,7 @@
-window.BASE_URL='https://alvarstrading.com';
-window.ADMIN_URL='https://alvarstrading.com';
-window.SHOP_URL='https://alvarstrading.com/';
-window.THEME_URL='https://alvarstrading.com/theme/';
+window.BASE_URL='https://arvandguarantee.shop';
+window.ADMIN_URL='https://arvandguarantee.shop';
+window.SHOP_URL='https://arvandguarantee.shop/';
+window.THEME_URL='https://arvandguarantee.shop/theme/';
 // window.BASE_URL='http://fixche.shop';
 // window.ADMIN_URL='http://fixche.shop';
 // window.SHOP_URL='http://fixche.shop/';
