055a51713419a59b13c013f6e198aa5ea4e40be7
Hamid Alinia <a.goodlookingboy@gmail.com>
2023-03-07 13:30:33 +0330

    front

--- src/app/defaultFront.mjs
+++ src/app/defaultFront.mjs
@@ -166,15 +166,15 @@ export default [
                 }
                 res.ssrParse().then(body => {
                     body = body.replace('</head>', `<title>${(hea.title && hea.title[req.headers.lan]) ? hea.title[req.headers.lan] : 'Nodeeweb'}</title></head>`);
-                    if (hea && hea.description)
-                        body = body.replace('</head>', `<meta name="description" content="${hea.description}" /></head>`);
-
+                    if (hea && hea.description && hea.description[req.headers.lan]) {
+                        body = body.replace('</head>', `<meta name="description" content="${hea.description[req.headers.lan]}" /></head>`);
+                    }
                     body = body.replace('</head>', `<link rel="canonical" href="${process.env.SHOP_URL}" /></head>`);
                     body = body.replace('</head>', `<meta property="og:locale" content="fa_IR" /></head>`);
                     body = body.replace('</head>', `<meta property="og:type" content="website" /></head>`);
                     body = body.replace('</head>', `<meta property="og:title" content="${(hea.title && hea.title[req.headers.lan]) ? hea.title[req.headers.lan] : 'Nodeeweb'}" /></head>`);
-                    if (hea && hea.description)
-                        body = body.replace('</head>', `<meta property="og:description" content="${hea.description}" /></head>`);
+                    if (hea && hea.description && hea.description[req.headers.lan])
+                        body = body.replace('</head>', `<meta property="og:description" content="${hea.description[req.headers.lan]}" /></head>`);
                     body = body.replace('</head>', `<meta property="og:url" content="${process.env.SHOP_URL}" /></head>`);
                     body = body.replace('</head>', `<meta property="og:site_name" content="${(hea.factore_shop_name) ? hea.factore_shop_name : 'Nodeeweb'}" /></head>`);
 
@@ -219,6 +219,174 @@ export default [
         "controller": (req, res, next) => {
             console.log('show front, go visit ', process.env.SHOP_URL);
             res.show()
+        },
+
+    },
+    {
+        "path": "/p/:_id",
+        "method": "get",
+        "access": "customer_all",
+        "controller": (req, res, next) => {
+            console.log('show /p/:_id, /defaultFront.mjs line: 230 ', process.env.SHOP_URL);
+            const arrayMin = (arr) => {
+                if (arr && arr.length > 0)
+                    return arr.reduce(function (p, v) {
+                        return (p < v ? p : v);
+                    });
+            };
+            let obj = {};
+            // if (req.mongoose.isValidObjectId(req.params._slug)) {
+            //     obj["_id"] = req.params._slug;
+            // } else {
+            //     obj["slug"] = req.params._slug;
+            //
+            // }
+            if (req.mongoose.isValidObjectId(req.params._id)) {
+                obj["_id"] = req.params._id;
+                // delete obj["slug"];
+            }
+            let Product = req.mongoose.model('Product');
+            let Settings = req.mongoose.model('Settings');
+            console.log('obj', obj)
+            Settings.findOne({}, "header_last", function (err, hea) {
+                console.log('hea', hea)
+                Product.findOne(obj, "title metadescription metatitle keywords excerpt type price in_stock salePrice combinations thumbnail photos slug labels _id",
+                    function (err, product) {
+                        if (err || !product) {
+                            // resolve({});
+                            return res.json({
+                                success: false,
+                                error: err
+                            });
+                        }
+                        let in_stock = "outofstock";
+                        let product_price = 0;
+                        let product_old_price = 0;
+                        let product_prices = [];
+                        let product_sale_prices = [];
+                        if (product.type === "variable") {
+                            if (product.combinations)
+                                _.forEach(product.combinations, (c) => {
+                                    if (c.in_stock) {
+                                        in_stock = "instock";
+                                        product_prices.push(parseInt(c.price) || 1000000000000);
+                                        product_sale_prices.push(parseInt(c.salePrice) || 1000000000000);
+                                    }
+
+                                });
+                            // console.log("gfdsdf");
+                            // console.log(product_prices);
+                            // console.log(product_sale_prices);
+                            let min_price = arrayMin(product_prices);
+                            let min_sale_price = arrayMin(product_sale_prices);
+                            // console.log("min_price", min_price);
+                            // console.log("min_sale_price", min_sale_price);
+                            product_price = min_price;
+                            if (min_sale_price > 0 && min_sale_price < min_price) {
+                                product_price = min_sale_price;
+                                product_old_price = min_price;
+                            }
+                        }
+                        if (product.type === "normal") {
+                            if (product.in_stock) {
+                                in_stock = "instock";
+                            }
+                            if (product.price) {
+                                product_price = product.price;
+                            }
+                            if (product.price && product.salePrice) {
+                                product_price = product.salePrice;
+                                product_old_price = product.price;
+                            }
+                        }
+
+                        // product.title = product['title'][req.headers.lan] || '';
+                        // product.description = '';
+                        // console.log(c);
+                        // });
+                        delete product.data;
+                        delete product.transaction;
+                        console.log(" product", product);
+                        let img = '';
+                        if (product.photos && product.photos[0]) {
+                            img = product.photos[0]
+
+                        }
+                        if (product.thumbnail) {
+                            img = product.thumbnail
+                        }
+
+                        let obj = {
+                            _id: product._id,
+                            product_price: product_price || "",
+                            product_old_price: product_old_price || "",
+                            availability: in_stock || "",
+                            image: img,
+                            keywords: "",
+                            metadescription: "",
+                        };
+                        if (product["keywords"]) {
+                            obj["keywords"] = product["keywords"][req.headers.lan] || product["keywords"];
+
+                        }
+                        if (product["metadescription"]) {
+                            obj["metadescription"] = product["metadescription"][req.headers.lan] || product["metadescription"];
+
+                        }
+                        if (product["title"]) {
+                            obj["title"] = product["title"][req.headers.lan] || product["title"];
+                        } else {
+                            obj["title"] = "";
+                        }
+                        if (product["title"]) {
+                            obj["product_name"] = product["title"][req.headers.lan] || product["title"];
+                        } else {
+                            obj["product_name"] = "";
+                        }
+                        if (product["description"]) {
+                            obj["description"] = product["description"][req.headers.lan] || product["description"];
+                        } else {
+                            obj["description"] = "";
+                        }
+                        if (product["slug"]) {
+                            obj["slug"] = product["slug"];
+                        }
+                        if (product["labels"]) {
+                            obj["labels"] = product["labels"];
+                        }
+                        if (!obj.metadescription) {
+                            obj.metadescription = obj["description"]
+                        }
+                        let mainTitle=obj.title;
+                        if(product.metatitle && product.metatitle[req.headers.lan]){
+                            mainTitle=product.metatitle[req.headers.lan]
+                        }
+                        res.ssrParse().then(body => {
+                            body = body.replace('</head>', `<title>${mainTitle}</title></head>`);
+                            body = body.replace('</head>', `<meta name="description" content="${obj.metadescription}" /></head>`);
+                            body = body.replace('</head>', `<link rel="canonical" href="${process.env.SHOP_URL}product/${req.params._id}/${req.params._slug}/" /></head>`);
+                            body = body.replace('</head>', `<meta name="product_id" content="${obj._id}" /></head>`);
+                            body = body.replace('</head>', `<meta name="product_name" content="${obj.product_name}" /></head>`);
+                            body = body.replace('</head>', `<meta name="product_price" content="${obj.product_price}" /></head>`);
+                            body = body.replace('</head>', `<meta name="product_old_price" content="${obj.product_old_price}" /></head>`);
+                            body = body.replace('</head>', `<meta name="product_image" content="/${obj.image}" /></head>`);
+                            body = body.replace('</head>', `<meta name="image" content="/${obj.image}" /></head>`);
+                            body = body.replace('</head>', `<meta name="availability" content="${obj.availability}" /></head>`);
+                            body = body.replace('</head>', `<meta name="og:image" content="/${obj.image}" /></head>`);
+                            body = body.replace('</head>', `<meta name="og:image:secure_url" content="/${obj.image}" /></head>`);
+                            body = body.replace('</head>', `<meta name="og:image:width" content="1200" /></head>`);
+                            body = body.replace('</head>', `<meta name="og:image:height" content="675" /></head>`);
+                            body = body.replace('</head>', `<meta name="og:locale" content="fa_IR" /></head>`);
+                            body = body.replace('</head>', `<meta name="og:type" content="website" /></head>`);
+                            body = body.replace('</head>', `<meta name="og:title" content="${mainTitle}" /></head>`);
+                            body = body.replace('</head>', `<meta name="og:description" content="${obj.description}" /></head>`);
+                            body = body.replace('</head>', `<meta name="og:url" content="." /></head>`);
+                            body = body.replace('</head>', (hea && hea.header_last) ? hea.header_last : "" + `</head>`);
+
+                            res.status(200).send(body);
+                        })
+                    }).lean();
+            })
         },
 
     },
@@ -532,8 +700,9 @@ export default [
                         }
                         let mainTitle=obj.title;
                         if(product.metatitle){
-                            mainTitle=product.metatitle
+                            mainTitle=product.metatitle[req.headers.lan] ? product.metatitle[req.headers.lan] : obj.title
                         }
+                        console.log('mainTitle',mainTitle)
                         res.ssrParse().then(body => {
                             body = body.replace('</head>', `<title>${mainTitle}</title></head>`);
                             body = body.replace('</head>', `<meta name="description" content="${obj.metadescription}" /></head>`);
@@ -552,9 +721,9 @@ export default [
                             body = body.replace('</head>', `<meta name="og:locale" content="fa_IR" /></head>`);
                             body = body.replace('</head>', `<meta name="og:type" content="website" /></head>`);
                             body = body.replace('</head>', `<meta name="og:title" content="${mainTitle}" /></head>`);
-                            body = body.replace('</head>', `<meta name="og:description" content="${obj.description}" /></head>`);
+                            body = body.replace('</head>', `<meta name="og:description" content="${obj.metadescription}" /></head>`);
                             body = body.replace('</head>', `<meta name="og:url" content="." /></head>`);
-                            body = body.replace('</head>', `<script type="application/ld+json">{"@context": "https://schema.org/","@type": "Product","name": "${mainTitle}","image": ["${process.env.SHOP_URL}${obj.image}"],"description": "${obj.description}","offers": {"@type": "Offer","url": "${process.env.SHOP_URL}product/${req.params._slug}","priceCurrency":"IRR","price": "${obj.product_price}","priceValidUntil":"2024-07-22","availability": "https://schema.org/InStock","itemCondition": "https://schema.org/NewCondition"}}</script></head>`);
+                            body = body.replace('</head>', `<script type="application/ld+json">{"@context": "https://schema.org/","@type": "Product","name": "${mainTitle}","image": ["${process.env.SHOP_URL}${obj.image}"],"description": "${obj.metadescription}","offers": {"@type": "Offer","url": "${process.env.SHOP_URL}product/${req.params._slug}","priceCurrency":"IRR","price": "${obj.product_price}","priceValidUntil":"2024-07-22","availability": "https://schema.org/InStock","itemCondition": "https://schema.org/NewCondition"}}</script></head>`);
                             body = body.replace('</head>', (hea && hea.header_last) ? hea.header_last : "" + `</head>`);
 
                             res.status(200).send(body);
