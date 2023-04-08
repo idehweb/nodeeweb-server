cde10d3ef477b114d1fcdc3f84ac023d9b52346c
Hamid Alinia <a.goodlookingboy@gmail.com>
2023-04-08 12:39:14 +0330

    update form fields

--- admin/site_setting/config.js
+++ admin/site_setting/config.js
@@ -4,8 +4,14 @@
 // window.SHOP_URL='https://parts.arvandguarantee.shop/';
 // window.THEME_URL='https://parts.arvandguarantee.shop/theme/';
 
-window.BASE_URL='https://alvarstrading.com';
-window.ADMIN_URL='https://alvarstrading.com';
-window.ADMIN_ROUTE='https://alvarstrading.com/admin';
-window.SHOP_URL='https://alvarstrading.com/';
-window.THEME_URL='https://alvarstrading.com/theme/';
+window.BASE_URL='https://felafel.cafe';
+window.ADMIN_URL='https://felafel.cafe';
+window.ADMIN_ROUTE='https://felafel.cafe/admin';
+window.SHOP_URL='https://felafel.cafe/';
+window.THEME_URL='https://felafel.cafe/theme/';
+
+// window.BASE_URL='https://alvarstrading.com';
+// window.ADMIN_URL='https://alvarstrading.com';
+// window.ADMIN_ROUTE='https://alvarstrading.com/admin';
+// window.SHOP_URL='https://alvarstrading.com/';
+// window.THEME_URL='https://alvarstrading.com/theme/';
