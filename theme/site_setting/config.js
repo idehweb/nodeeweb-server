e1d970439c147a22730b83ad86b046117f81337f
Hamid Alinia <a.goodlookingboy@gmail.com>
2023-04-15 12:32:50 +0330

    remove bugs

--- theme/site_setting/config.js
+++ theme/site_setting/config.js
@@ -4,6 +4,13 @@ window.ADMIN_ROUTE='https://parts.arvandguarantee.shop/admin';
 window.SHOP_URL='https://parts.arvandguarantee.shop/';
 window.THEME_URL='https://parts.arvandguarantee.shop/theme/';
 
+// window.BASE_URL='https://fixche.shop';
+// window.ADMIN_URL='https://fixche.shop';
+// window.ADMIN_ROUTE='https://fixche.shop/admin';
+// window.SHOP_URL='https://fixche.shop/';
+// window.THEME_URL='https://fixche.shop/theme/';
+
+
 // window.BASE_URL='https://felafel.cafe';
 // window.ADMIN_URL='https://felafel.cafe';
 // window.ADMIN_ROUTE='https://felafel.cafe/admin';
