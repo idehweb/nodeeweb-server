e3c05d853796891d10f82ea28b67d53f34c3b65b
Hamid Alinia <a.goodlookingboy@gmail.com>
2023-05-15 11:55:50 +0330

    add icons of product-single

--- admin/site_setting/config.js
+++ admin/site_setting/config.js
@@ -1,8 +1,8 @@
-// window.BASE_URL='https://parts.arvandguarantee.shop';
-// window.ADMIN_URL='https://parts.arvandguarantee.shop';
-// window.ADMIN_ROUTE='https://parts.arvandguarantee.shop/admin';
-// window.SHOP_URL='https://parts.arvandguarantee.shop/';
-// window.THEME_URL='https://parts.arvandguarantee.shop/theme/';
+window.BASE_URL='https://parts.arvandguarantee.shop';
+window.ADMIN_URL='https://parts.arvandguarantee.shop';
+window.ADMIN_ROUTE='https://parts.arvandguarantee.shop/admin';
+window.SHOP_URL='https://parts.arvandguarantee.shop/';
+window.THEME_URL='https://parts.arvandguarantee.shop/theme/';
 
 // window.BASE_URL='https://arvandguarantee.shop';
 // window.ADMIN_URL='https://arvandguarantee.shop';
@@ -29,8 +29,8 @@
 // window.SHOP_URL='https://alvarstrading.com/';
 // window.THEME_URL='https://alvarstrading.com/theme/';
 
-window.BASE_URL='https://gomrok24.com';
-window.ADMIN_URL='https://gomrok24.com';
-window.ADMIN_ROUTE='https://gomrok24.com/admin';
-window.SHOP_URL='https://gomrok24.com/';
-window.THEME_URL='https://gomrok24.com/theme/';
+// window.BASE_URL='https://gomrok24.com';
+// window.ADMIN_URL='https://gomrok24.com';
+// window.ADMIN_ROUTE='https://gomrok24.com/admin';
+// window.SHOP_URL='https://gomrok24.com/';
+// window.THEME_URL='https://gomrok24.com/theme/';
